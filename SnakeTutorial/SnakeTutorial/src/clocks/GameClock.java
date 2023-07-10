7907719bb60f4419f75f6e4b02284e0184db72b2
EmmanuelsMUM <137266152+EmmanuelsMUM@users.noreply.github.com>
2023-07-10 08:45:11 +0200

    Add files via upload

---
+++ SnakeTutorial/SnakeTutorial/src/clocks/GameClock.java
@@ -0,0 +1,54 @@
+package clocks;
+
+import actions.Collision;
+import game.Snake;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import static game.Snake.gift;
+
+public class GameClock extends Thread{
+    public static boolean running = true;
+public int w= 100;
+public int tick=0;
+public void setW(int w){
+    this.w = w;
+}
+    public void run(){
+        while(running){
+            try {
+                tick++;
+                if(tick >100){
+                    this.setW(100);
+                }
+                sleep(w);
+                Snake.move();
+                Snake.waitToMove = false;
+                Collision.collidePickUp();
+                if(Collision.collideGift()){
+                    Snake.tails.clear();
+                    Snake.score = 0;
+                }
+                if(Collision.collideKoks()){
+                    this.setW(50);
+                    tick = 0;
+                }
+                if(Collision.collideSelf()){
+                    Snake.tails.clear();
+                    Snake.score = 0;
+                    this.setW(100);
+                }
+                if(Collision.collideWall()){
+                    Snake.tails.clear();
+                    Snake.head.setX(7);
+                    Snake.head.setY(7);
+                    Snake.score = 0;
+                    this.setW(100);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
