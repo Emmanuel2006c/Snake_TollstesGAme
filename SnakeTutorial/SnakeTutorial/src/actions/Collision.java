6f0dc215adb6b841cee2ffb5425b94dd53dae9b8
Fe.Kamm <Fe.Kamm@schulen.local>
2023-07-10 08:57:11 +0200

    Hallo Emmanuel

--- SnakeTutorial/SnakeTutorial/src/actions/Collision.java
+++ SnakeTutorial/SnakeTutorial/src/actions/Collision.java
@@ -4,7 +4,7 @@ import clocks.GameClock;
 import game.Snake;
 
 import java.util.concurrent.ThreadLocalRandom;
-
+//hallo Emmanuel
 public class Collision {
 
     public static boolean collideSelf() {
